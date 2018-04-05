4cead1bed59e1fbe29d130bf191533cf05609653
renatocabreraf <renatojose98@yahoo.com>
2018-04-04 18:23:15 -0600

    Agregar archivos de proyecto.

---
+++ Lab3/DBContext/DefaultConnection.cs
@@ -0,0 +1,49 @@
+﻿using Lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TDA.Clases;
+
+namespace Lab3.DBContext
+{
+    public class DefaultConnection
+    {
+        private static volatile DefaultConnection Instance;
+        private static object syncRoot = new Object();
+
+        public List<Partido> listaPartido = new List<Partido>();
+
+        public ArbolAVL<Partido, int> arbolNoPartido = new ArbolAVL<Partido, int>();
+        public List<Partido> listaNoPartido = new List<Partido>();
+
+        public ArbolAVL<Partido, DateTime> arbolFechaPartido = new ArbolAVL<Partido, DateTime>();
+        public List<Partido> listaFechaPartido = new List<Partido>();
+
+
+        public int IDActual { get; set; }
+
+        private DefaultConnection()
+        {
+            IDActual = 0;
+        }
+
+        public static DefaultConnection getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new DefaultConnection();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+    }
+}
